dcb9f4cf06b53a925c8bbc251b2e6ee503bb1e5a
Daniel <Daniel.Huson@uni-tuebingen.de>
2019-08-01 07:23:48 +0200

    fixed bug in SVG export

--- src/module-info.java
+++ src/module-info.java
@@ -7,17 +7,7 @@ module dendroscope {
     requires commons.collections;
     requires nexml;
 
-    exports dendroscope.algorithms.clusternet;
-    exports dendroscope.algorithms.gallnet;
-    exports dendroscope.algorithms.levelknet;
-    exports dendroscope.algorithms.levelknet.cass;
-    exports dendroscope.algorithms.levelknet.leo;
-    exports dendroscope.algorithms.levelknet.tmp;
-    exports dendroscope.algorithms.utils;
-    exports dendroscope.anticonsensus;
-    exports dendroscope.autumn;
-    exports dendroscope.autumn.hybridnetwork;
-    exports dendroscope.autumn.hybridnumber;
+
     exports dendroscope.commands;
     exports dendroscope.commands.autumn;
     exports dendroscope.commands.collapse;
@@ -30,31 +20,7 @@ module dendroscope {
     exports dendroscope.commands.hybroscale;
     exports dendroscope.commands.mul;
     exports dendroscope.commands.select;
-    exports dendroscope.consensus;
-    exports dendroscope.core;
-    exports dendroscope.dialogs.input;
-    exports dendroscope.drawer;
-    exports dendroscope.dtl;
-    exports dendroscope.embed;
-    exports dendroscope.hybrid;
-    exports dendroscope.hybroscale.controller;
-    exports dendroscope.hybroscale.model;
-    exports dendroscope.hybroscale.model.attachNetworks;
-    exports dendroscope.hybroscale.model.cmpAllMAAFs;
-    exports dendroscope.hybroscale.model.cmpMinNetworks;
-    exports dendroscope.hybroscale.model.parallelization;
-    exports dendroscope.hybroscale.model.reductionSteps;
-    exports dendroscope.hybroscale.model.treeObjects;
-    exports dendroscope.hybroscale.model.util;
-    exports dendroscope.hybroscale.rerooting;
-    exports dendroscope.hybroscale.terminals;
-    exports dendroscope.hybroscale.util;
-    exports dendroscope.hybroscale.util.graph;
-    exports dendroscope.hybroscale.util.lcaQueries;
-    exports dendroscope.hybroscale.util.newick;
-    exports dendroscope.hybroscale.util.sparseGraph;
-    exports dendroscope.hybroscale.view;
-    exports dendroscope.io;
+
     exports dendroscope.io.nexml;
     exports dendroscope.main;
 
