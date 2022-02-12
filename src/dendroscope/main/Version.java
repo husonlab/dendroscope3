36693f863c39c6ef65b343d332b9e4108f099d31
Daniel <daniel.huson@uni-tuebingen.de>
2022-02-12 12:07:00 +0100

    new version

--- src/dendroscope/main/Version.java
+++ src/dendroscope/main/Version.java
@@ -26,6 +26,6 @@ package dendroscope.main;
  */
 public class Version {
     public static final String NAME = "Dendroscope";
-	public static final String SHORT_DESCRIPTION = "Dendroscope (version 3.7.6, built 25 Aug 2021)";
+	public static final String SHORT_DESCRIPTION = "Dendroscope (version 3.8.1, built 12 Feb 2022)";
 
 }
