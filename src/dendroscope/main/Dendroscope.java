803f09f67fb28295f4313a9337c7aaf82c634a60
Daniel <daniel.huson@uni-tuebingen.de>
2023-03-25 14:05:45 +0800

    updated copyright

--- src/dendroscope/main/Dendroscope.java
+++ src/dendroscope/main/Dendroscope.java
@@ -1,20 +1,20 @@
 /*
- *   Dendroscope.java Copyright (C) 2023 Daniel H. Huson
+ * Dendroscope.java Copyright (C) 2023 Daniel H. Huson
  *
- *   (Some files contain contributions from other authors, who are then mentioned separately.)
+ * (Some files contain contributions from other authors, who are then mentioned separately.)
  *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package dendroscope.main;
 
@@ -78,7 +78,7 @@ public class Dendroscope {
         final var options = new ArgsOptions(args, this, "Program for rooted phylogenetic trees and networks");
         options.setAuthors("Daniel H. Huson, with some contributions from other authors");
         options.setVersion(ProgramProperties.getProgramVersion());
-        options.setLicense("Copyright (C) 2023 Daniel H. Huson. This program comes with ABSOLUTELY NO WARRANTY.\n" +
+        options.setLicense("Copyright (C) 2023  Daniel H. Huson. This program comes with ABSOLUTELY NO WARRANTY.\n" +
                            "This is free software, licensed under the terms of the GNU General Public License, Version 3.");
         options.setVersion(ProgramProperties.getProgramVersion());
 
